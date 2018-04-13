8242f4e091fa1ce45e62c183686c80e53470b010
Seong Hoon Seo <andyseo247@gmail.com>
2018-04-13 23:38:59 +0900

    implemented methods for copying random files and creating labels

---
+++ file_control/random_select.py
@@ -0,0 +1,33 @@
+# Referenced https://stackoverflow.com/questions/9588257/selecting-and-copying-a-random-file-several-times
+
+import os
+import shutil
+import random
+import os.path
+
+#src_dir = '/data/Bodypart/SKULL'
+#target_dir = '/data/Bodypart/sample/Skull/images'
+
+target_num = 500
+
+src_files = (os.listdir(src_dir))
+def valid_path(dir_path, filename):
+    full_path = os.path.join(dir_path, filename)
+    return os.path.isfile(full_path)
+
+filenames_list = [f for f in src_files if valid_path(src_dir, f)]  
+
+choices = 0
+print("Number of files in directory: ", len(filenames_list))
+
+if target_num < len(filenames_list):
+    choices = random.sample(filenames_list, target_num)
+    print("Copying" , target_num, "random files from directory...")
+else:
+    choices = filenames_list
+    print("Copying all" , len(filenames_list), "files from directory...")
+
+for filename in choices:
+    file = os.path.join(src_dir, filename)
+    shutil.copy(file, target_dir)
+print ('Finished!')
