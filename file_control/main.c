ed9f895fc2addd6611c4488279c7d8804fc9c9a4
Yonghoon Park <jim6829@naver.com>
2018-04-14 00:55:35 +0900

    add mv

--- file_control/main.c
+++ file_control/main.c
@@ -12,6 +12,8 @@
 
 char* FILELIST = "./filelist.txt";
 char* INPUTLIST = "./inputlist.txt";
+char* DESTINATION = "../";
+int BUF_SIZE = 128;
 
 void ls() {
 	char* argv[] = {"/bin/ls", NULL};
@@ -36,9 +38,9 @@ void ls() {
 
 int lst_num() {
 	FILE* fp_filelist = fopen(FILELIST, "r");
-	char buf[128];
+	char buf[BUF_SIZE];
 	int result = 0;
-	while (fgets(buf, 128, fp_filelist)) result++;
+	while (fgets(buf, BUF_SIZE, fp_filelist)) result++;
 	fclose(fp_filelist);
 	return result - 4;
 }
@@ -56,19 +58,51 @@ int* rand_arr(int n) {
 	return p;
 }
 
-void slct(int* idx, int n, int m) {
+int slct(int* idx, int n, int m) {
 	FILE* fp_filelist = fopen(FILELIST, "r");
 	FILE* fp_inputlist = fopen(INPUTLIST, "w");
 	char** buf = (char **) malloc(n * sizeof(char*));
-	for (int i = 0; i != n; i++) buf[i] = (char *) malloc(128 * sizeof(char));
-	for (int i = 0; i != n; i++) fgets(buf[idx[i]], 128, fp_filelist);
-	int x = n < m ? n : m;
-	for (int i = 0; i != x; i++) fputs(buf[i], fp_inputlist);
+	for (int i = 0; i != n; i++) buf[i] = (char *) malloc(BUF_SIZE * sizeof(char));
+	for (int i = 0; i != n; i++) fgets(buf[idx[i]], BUF_SIZE, fp_filelist);
+	int length = n < m ? n : m;
+	for (int i = 0; i != length; i++) fputs(buf[i], fp_inputlist);
 	free(idx);
 	for (int i = 0; i != n; i++) free(buf[i]);
 	free(buf);
 	fclose(fp_filelist);
 	fclose(fp_inputlist);
+
+	return length;
+}
+
+void mv(int length) {
+	char* argv[] = {"/bin/mv", NULL, DESTINATION, NULL};
+	char* env[] = {NULL};
+	FILE* fp = fopen(INPUTLIST, "r");
+	int status;
+	char** buf = (char **) malloc(length * sizeof(char*));
+	for (int i = 0; i != length; i++) buf[i] = (char *) malloc(BUF_SIZE * sizeof(char));
+
+	for (int i = 0; i != length; i++) {
+		fgets(buf[i], BUF_SIZE, fp);
+		int len = strlen(buf[i]);
+		if (len > 0 && buf[i][len - 1] == '\n') buf[i][len - 1] = '\0';
+	}
+	fclose(fp);
+
+	for (int i = 0; i != length; i++) {
+		if (fork() == 0) {
+			argv[1] = buf[i];
+			if (execve(argv[0], argv, env) < 0) {
+				printf("Error\n");
+				exit(0);
+			}
+		}
+	}
+	for (int i = 0; i != length; i++) wait(&status);
+
+	for (int i = 0; i != length; i++) free(buf[i]);
+	free(buf);
 
 	return;
 }
@@ -81,6 +115,7 @@ int main(int argc, char* argv[]) {
 	int num_arg = atoi(argv[1]);
 	ls();
 	int num_lst = lst_num();
-	slct(rand_arr(num_lst), num_lst, num_arg);
+	int length = slct(rand_arr(num_lst), num_lst, num_arg);
+	mv(length);
 	return 0;
 }
